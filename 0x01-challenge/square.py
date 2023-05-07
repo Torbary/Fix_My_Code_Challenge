3a9e22df5d05a771052e43d245bbc9a1671a8045
Torbary <torbarey020@gmail.com>
2023-05-07 21:52:04 +0100

    added __doc__

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""defines a area and PermiterOfMySquare"""
+
+
+class square():
+    """define a class `square`"""
+    
+    width = 0
+    height = 0
+
+    
+    def __init__(self, *args, **kwargs):
+        """instantiation of a square"""
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """perimeter of a square"""
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """print the string representation"""
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
